59afcfeb835f9334acffa9d0057beb90c4542cfa
Maker-Gitsune <109487688+Maker-Gitsune@users.noreply.github.com>
2023-03-11 03:29:18 -0500

    Code to support Peg added.
    Some assembly required. This is the bare minimum needed to use Peg. The default keymap is unusable in its current form; you will need to setup your own keymap.

---
+++ Splaynck code/Peg stuff/main.py
@@ -0,0 +1,26 @@
+from kb import KMKKeyboard
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+from kmk.modules.modtap import ModTap
+from kmk.hid import HIDModes
+from kmk.handlers.sequences import send_string
+import supervisor
+keyboard = KMKKeyboard()
+modtap = ModTap()
+layers_ext = Layers()
+keyboard.modules.append(layers_ext)
+keyboard.modules.append(modtap)
+keyboard.modules = [layers_ext, modtap]
+
+# keymap
+keyboard.keymap = [ [KC.ESCAPE,KC.Y,KC.P,KC.O,KC.U,KC.J,KC.K,KC.D,KC.L,KC.C,KC.W,KC.ENTER,KC.MT(KC.TAB, KC.LCTL, prefer_hold=True, tap_interrupted=False, tap_time=150),KC.I,KC.N,KC.E,KC.A,KC.COMMA,KC.M,KC.H,KC.T,KC.S,KC.R,KC.MT(KC.TAB, KC.LCTL, prefer_hold=True, tap_interrupted=False, tap_time=150),KC.CAPSLOCK,KC.Q,KC.Z,KC.SLASH,KC.DOT,KC.SCOLON,KC.F5,KC.B,KC.F,KC.G,KC.V,KC.X,KC.NO,KC.LGUI,KC.LALT,KC.DELETE,KC.RSHIFT,KC.MT(KC.SPC, KC.LSFT, prefer_hold=True, tap_interrupted=False, tap_time=150),KC.BSPC,KC.RALT,KC.RGUI,KC.NO], 
+[KC.NO], 
+[KC.NO], 
+[KC.NO], 
+[KC.NO], 
+[KC.NO], 
+[KC.NO], 
+[KC.NO] ] 
+# keymap
+if __name__ == '__main__': 
+    keyboard.go(hid_type=HIDModes.USB)
